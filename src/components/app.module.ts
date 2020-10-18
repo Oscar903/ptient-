68314a3c8000ade8e9f5e5e809afd1bb1193af0a
Oscar903 <72770818+Oscar903@users.noreply.github.com>
2020-10-18 13:13:31 -0500

    Add files via upload

---
+++ src/components/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { Routes, RouterModule, Router } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { PacienteComponent } from './components/paciente/paciente.component';
+import { HistorialComponent } from './components/historial/historial.component';
+
+const rutas: Routes = [
+  { path: 'registro', component: RegistroComponent },
+  { path: 'paciente', component: PacienteComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'historial', component: HistorialComponent },
+];
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    PacienteComponent,
+    HistorialComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(rutas)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
